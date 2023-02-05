788e160c1dd13736dfb397c5e2d15a127ac87bba
RuanSilva2167 <ruansilva6721@gmail.com>
2023-02-04 22:08:46 -0300

    adicionando footer e navbar ao projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+     <Footer/>
 
     </div>
   );
